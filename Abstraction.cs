fd50b0e1960d48d71e348383a063f9436a5da9d8
batuhan-uzun <batuhanuzun2356@gmail.com>
2024-09-17 20:13:45 +0300

    Add project files.

---
+++ Abstraction.cs
@@ -0,0 +1,11 @@
+﻿using AbstractionProject;
+
+Employee manager = new ProjectManager() // Created a ProjectManager object of type Employee
+{
+    Name = "Hasan",
+    Surname = "Çıldırmış",
+    Department = "Project Manager",
+};
+
+manager.DepartmentInfo(); // DepartmentInfo abstract method inherited from Employee in Manager object was executed
+
---
+++ Company.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractionProject
+{
+    public abstract class Employee
+    {
+        public string Name { get; set; } // 
+        public string Surname { get; set; } // Employee properties
+        public string Department { get; set; } //
+        public abstract void DepartmentInfo(); // Defined abstract method to use in different ways in each department
+    }
+    
+    public class SoftwareDeveloper : Employee
+    {
+        public override void DepartmentInfo() // Abstract method overridden for Software Developer
+        {
+            Console.WriteLine("I am working as a Software Developer");
+        }
+    }
+
+    public class ProjectManager : Employee
+    {
+        public override void DepartmentInfo() // Abstract method overridden for Project Manager
+        {
+            Console.WriteLine("I am working as a Project Manager");
+        }
+    }
+
+    public class SalesRepresentative : Employee
+    {
+        public override void DepartmentInfo() // Abstract method overridden for Sales Representative
+        {
+            Console.WriteLine("I am working as a Sales Representative");
+        }
+    }
+}
